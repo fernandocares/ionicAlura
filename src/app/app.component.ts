0a46fbb1225c21d7988ede24fe3239bff0f93fdf
Fernando Cares <fernandocareswork@gmail.com>
2019-05-18 15:14:53 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,87 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { ListaAgendamentosPage } from '../pages/lista-agendamentos/lista-agendamentos';
+import { LoginPage } from '../pages/login/login';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { UsuariosServiceProvider } from '../providers/usuarios-service/usuarios-service';
+import { OneSignal, OSNotification } from '@ionic-native/onesignal';
+import { AgendamentoDaoProvider } from '../providers/agendamento-dao/agendamento-dao';
+import { Agendamento } from '../modelos/agendamento';
+
 @Component({
+  selector: 'myapp',
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) public nav: Nav;
+  rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+  public paginas = [
+    { titulo: 'Agendamentos', componente: ListaAgendamentosPage.name, icone: 'calendar' },
+    { titulo: 'Perfil', componente: PerfilPage.name, icone: 'person' }
+  ];
+
+  constructor(platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private _usuariosService: UsuariosServiceProvider,
+    private _onesignal: OneSignal,
+    private _agendamentoDao: AgendamentoDaoProvider) {
+      platform.ready().then(() => {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        statusBar.styleDefault();
+        splashScreen.hide();
+
+        //configurar onesignal
+        let iosConfigs = {
+          kOSSettingsKeyAutoPrompt: true,
+          kOSSettingsKeyInAppLaunchURL: false
+        }
+
+        this._onesignal
+            .startInit('e53f5d24-40e4-458f-99db-5230cf3f8bc0', '97629632146')
+            .iosSettings(iosConfigs);
+
+        this._onesignal.inFocusDisplaying(
+          this._onesignal.OSInFocusDisplayOption.Notification
+        );
+
+        this._onesignal.handleNotificationReceived()
+            .subscribe(
+              (notificacao: OSNotification) => {
+                let dadosAdicionais = notificacao.payload
+                                                .additionalData;
+                let agendamentoId = dadosAdicionais['agendamento-id'];
+
+                this._agendamentoDao.recupera(agendamentoId)
+                    .subscribe(
+                      (agendamento: Agendamento) => {
+                        agendamento.confirmado = true;
+
+                        this._agendamentoDao.salva(agendamento);
+                      }
+                    )
+              }
+            );
+
+        this._onesignal.endInit();
+      });
+  }
+
+  irParaPagina(componente) {
+    this.nav.push(componente);
+  }
+
+  get avatar() {
+    return this._usuariosService.obtemAvatar();
+  }
+
+  get usuarioLogado() {
+    return this._usuariosService.obtemUsuarioLogado();
   }
 }
 
